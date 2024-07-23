0fb80675c7564eb05a40609e6ede554c9aacf473
Rowdy <mroddy5280@gmail.com>
2024-07-22 23:31:52 -0600

    fix(route.ts): fixed return value for route

--- app/api/sign-up/route.ts
+++ app/api/sign-up/route.ts
@@ -1,4 +1,4 @@
-import AirTable from "airtable";
+import AirTable, { type Record } from "airtable";
 import { NextResponse } from "next/server";
 
 const base = new AirTable({ apiKey: process.env.AIRTABLE_API_KEY }).base(
@@ -16,29 +16,18 @@ export async function POST(request: Request, response: Response) {
 	try {
 		const { email, phoneModel, previousCustomer, newToCrypto } = req;
 
-		const record = await new Promise((resolve, reject) => {
-			base("Table 1").create(
-				{
-					"Your email": email,
-					"Your phone model": phoneModel,
-					"Have you use eSIM before?": previousCustomer,
-					"Have you used crypto before?": newToCrypto,
-				},
-				// biome-ignore lint/suspicious/noExplicitAny: <explanation>
-				(err: Error | null, record: any | null) => {
-					if (err) {
-						console.error(err);
-						reject(err);
-					} else {
-						resolve(record);
-					}
-				},
-			);
+		const record = base("Table 1").create({
+			"Your email": email,
+			"Your phone model": phoneModel,
+			"Have you use eSIM before?": previousCustomer,
+			"Have you used crypto before?": newToCrypto,
 		});
-		if (!record) {
+
+		const data = await record;
+		if (!data) {
 			throw new Error("Failed to create record");
 		}
-		return NextResponse.json({ id: record.getId() });
+		return NextResponse.json({ id: data.getId() });
 	} catch (error) {
 		console.error(error);
 		return NextResponse.json(
