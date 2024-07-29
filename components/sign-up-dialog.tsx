d02ffcc8c362897d439a087070014d3417f37b16
Rowdy <mroddy5280@gmail.com>
2024-07-29 00:34:59 -0600

    fix(signup-modal): qa for signup modal

--- components/sign-up-dialog.tsx
+++ components/sign-up-dialog.tsx
@@ -1,43 +1,49 @@
-"use client";
-import type React from "react";
-import { useState } from "react";
-import Image from "next/image";
+'use client'
+import type React from 'react'
+import { useState } from 'react'
+import Image from 'next/image'
 
-import Logo from "@/assets/logo.svg";
+import Logo from '@/assets/logo.svg'
 
-import { Button } from "@/components/ui/button";
+import { Button } from '@/components/ui/button'
 import {
-	Dialog,
-	DialogContent,
-	DialogHeader,
-	DialogTitle,
-	DialogTrigger,
-} from "@/components/ui/dialog";
-import { DialogDescription } from "@radix-ui/react-dialog";
-import { SignUpForm } from "./sign-up-form";
+    Dialog,
+    DialogContent,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from '@/components/ui/dialog'
+import { DialogDescription } from '@radix-ui/react-dialog'
+import { SignUpForm } from './sign-up-form'
 
 const SignUpDialog = ({
-	open,
-	setOpen,
+    open,
+    setOpen,
 }: {
-	open: boolean;
-	setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+    open: boolean
+    setOpen: React.Dispatch<React.SetStateAction<boolean>>
 }) => {
-	return (
-		<Dialog open={open} onOpenChange={setOpen}>
-			<DialogContent className="rounded-3xl w-11/12">
-				<DialogHeader>
-					<DialogTitle className="flex justify-start -mt-2">
-						<Image src={Logo} alt="Logo" width={100} height={100} />
-					</DialogTitle>
-					<DialogDescription className="flex justify-start">
-						Beta Sign Up
-					</DialogDescription>
-				</DialogHeader>
-				<SignUpForm setOpen={setOpen} />
-			</DialogContent>
-		</Dialog>
-	);
-};
+    return (
+        <Dialog open={open} onOpenChange={setOpen}>
+            <DialogContent className="rounded-3xl w-11/12">
+                <DialogHeader>
+                    <DialogTitle className="flex justify-start">
+                        <Image
+                            src={Logo}
+                            alt="Logo"
+                            width={100}
+                            height={100}
+                            className="h-6"
+                        />
+                    </DialogTitle>
+                    <DialogDescription className="flex justify-start text-2xl text-outer-space-950 font-bold font-heading pt-14">
+                        Beta Sign Up
+                    </DialogDescription>
+                </DialogHeader>
+                <SignUpForm setOpen={setOpen} />
+            </DialogContent>
+        </Dialog>
+    )
+}
 
-export default SignUpDialog;
+export default SignUpDialog
