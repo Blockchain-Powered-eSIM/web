4c59ab7d4a78e9272696008630cf1a92c0214131
Rowdy <mroddy5280@gmail.com>
2024-07-31 12:11:44 -0600

    feat(layout.tsx): added script to track analytics to main layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,48 +11,69 @@ import { cn } from "@/lib/utils";
 import { NavBar } from "@/components/global/nav-bar";
 import { Footer } from "@/components/global/footer";
 import { Toaster } from "@/components/ui/toaster";
+import Script from "next/script";
 
 const anybody = Anybody({
-	subsets: ["latin"],
-	display: "swap",
-	variable: "--font-anybody",
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-anybody",
 });
 
 const lexend = Lexend({
-	subsets: ["latin"],
-	display: "swap",
-	variable: "--font-lexend",
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-lexend",
 });
 
 export const metadata: Metadata = {
-	title: "Kokio",
-	description: "Blockchain powered eSIM",
+  title: "Kokio",
+  description: "Blockchain powered eSIM",
 };
 
 export default function RootLayout({
-	children,
+  children,
 }: Readonly<{
-	children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-	const initialState = cookieToInitialState(config, headers().get("cookie"));
-	return (
-		<html
-			lang="en"
-			suppressHydrationWarning
-			className={cn(anybody.variable, lexend.variable)}
-		>
-			<body
-				className={cn(
-					"min-h-screen font-sans antialiased flex flex-col bg-beach-sky",
-				)}
-			>
-				<Web3ModalProvider initialState={initialState}>
-					<NavBar />
-					<div className="flex-1 m-0 p-0">{children}</div>
-					<Footer />
-					<Toaster />
-				</Web3ModalProvider>
-			</body>
-		</html>
-	);
+  const initialState = cookieToInitialState(config, headers().get("cookie"));
+  return (
+    <html
+      lang="en"
+      suppressHydrationWarning
+      className={cn(anybody.variable, lexend.variable)}
+    >
+      <body
+        className={cn(
+          "min-h-screen font-sans antialiased flex flex-col bg-beach-sky"
+        )}
+      >
+        <Web3ModalProvider initialState={initialState}>
+          <NavBar />
+          <div className="flex-1 m-0 p-0">{children}</div>
+          <Footer />
+          <Toaster />
+        </Web3ModalProvider>
+        <Script
+          id="matomo-tracking"
+          strategy="afterInteractive"
+          // biome-ignore lint/security/noDangerouslySetInnerHtml: <explanation>
+          dangerouslySetInnerHTML={{
+            __html: `
+							  var _paq = window._paq = window._paq || [];
+								/* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+								_paq.push(['trackPageView']);
+								_paq.push(['enableLinkTracking']);
+								(function() {
+									var u="https://psedev.matomo.cloud/";
+									_paq.push(['setTrackerUrl', u+'matomo.php']);
+									_paq.push(['setSiteId', '17']);
+									var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+									g.async=true; g.src='https://cdn.matomo.cloud/psedev.matomo.cloud/matomo.js'; s.parentNode.insertBefore(g,s);
+								})();
+						`,
+          }}
+        />
+      </body>
+    </html>
+  );
 }
