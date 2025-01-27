f98ee3f5305320335ec5d03fe829c5333b0e7b65
GuyPhy <guyphy@gmms.xyz>
2025-01-27 17:17:48 +0530

    Apply prettier config

--- components/road-map.tsx
+++ components/road-map.tsx
@@ -3,10 +3,7 @@ import React from "react";
 const roadMapData = [
   {
     title: "Jan",
-    milestones: [
-      "Finalize audit",
-      "Conduct QA internal testing",
-    ],
+    milestones: ["Finalize audit", "Conduct QA internal testing"],
   },
   {
     title: "Feb",
