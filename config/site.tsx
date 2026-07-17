e7830e50acd8611c9b26572d9de9801dc9091586
GuyPhy <tgoel0499@gmail.com>
2026-07-17 12:23:34 +0530

    add SEO metadata components

--- config/site.tsx
+++ config/site.tsx
@@ -5,6 +5,7 @@ import Docs from "@/assets/icons/docs-fill.svg";
 
 export const siteConfig = {
   name: "Kokio",
+  url: "https://kokio.app",
   header: "Experience the Future of Global Connectivity",
   description:
     "Travel with confidence with Kokio’s travel data plans. Unlike traditional eSIM providers, Kokio leverages cutting-edge blockchain technology for enhanced security, privacy, and ease of use. Enjoy seamless connectivity across over 200 destinations worldwide.",
